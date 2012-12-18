f1d3c1ed0ca5d0ecd815f1ba64fa3f9fabfe29a1
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2012-12-18 15:27:00 -0800

    Update the various config scripts

--- python/test_all.sh
+++ python/test_all.sh
@@ -3,6 +3,9 @@
 # you can source the setup_afids_env.sh file and then run nosetests
 
 export PYTHONPATH=${python_test_path}
-export SPICEDATA=$(AFIDS_DATA)/cspice
+source ${AFIDS_DATA}/setup_afids_data.sh
+export PATH=${python_exec_path}
+echo "Hi there"
+echo ${PATH}
 nosetests ${srcdir}/afids_python/python/*/ 
 

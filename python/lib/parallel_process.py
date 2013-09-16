dfafe7f16dfb0396f827342cf3c2e08e856bbc0d
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2013-09-16 13:10:30 -0700

    Continue working on write_image

--- python/lib/parallel_process.py
+++ python/lib/parallel_process.py
@@ -4,7 +4,11 @@ from functools import partial
 import numpy as np
 from multiprocessing import Pool
 
-def parallel_process_tile(img_in, lstart, sstart, nline, nsamp, out):
+def parallel_process_tile(img_in, lstart, sstart, nline, nsamp, out, 
+                          verbose = False):
+    if(verbose):
+        print "Processing (%d, %d) to (%d, %d)" % \
+            (lstart, sstart, lstart + nline, sstart + nsamp)
     if(out.dtype == np.float32 or out.dtype == np.float64):
         out[lstart:(lstart + nline), sstart:(sstart + nsamp)] = \
             img_in.read_double(lstart, sstart, nline, nsamp)
@@ -16,16 +20,20 @@ def do_parallel_process(p):
     p()
     return True
 
-def parallel_process_image(img_in, out, number_process):
-    process_nline = img_in.number_line / number_process
+def parallel_process_image(img_in, out, process_nline, process_nsamp, 
+                           number_process, verbose = False):
     process_list =  [ ] 
     for lstart in range(0, img_in.number_line, process_nline):
         tile_nline = process_nline
         if(lstart + tile_nline > img_in.number_line):
             tile_nline = img_in.number_line - lstart
-        job = partial(parallel_process_tile, img_in, lstart, 0,
-                      tile_nline, img_in.number_sample, out)
-        process_list.append(job)
+        for sstart in range(0, img_in.number_sample, process_nsamp):
+            tile_nsamp = process_nsamp
+            if(sstart + tile_nsamp > img_in.number_sample):
+                tile_nsamp = img_in.number_sample - sstart
+            job = partial(parallel_process_tile, img_in, lstart, sstart,
+                          tile_nline, tile_nsamp, out, verbose = verbose)
+            process_list.append(job)
     if(number_process ==1):
         res = map(do_parallel_process, process_list)
     else:

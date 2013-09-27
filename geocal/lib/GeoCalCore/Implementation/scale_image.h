001ec80e0686f287e53a9f11a7bf98be363e1f07
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2013-09-26 17:25:22 -0700

    Continue working on abachd

--- geocal/lib/GeoCalCore/Implementation/scale_image.h
+++ geocal/lib/GeoCalCore/Implementation/scale_image.h
@@ -1,6 +1,7 @@
 #ifndef SCALE_IMAGE_H
 #define SCALE_IMAGE_H
 #include "calc_raster.h"
+#include "calc_raster_multi_band.h"
 
 namespace GeoCal {
 /****************************************************************//**
@@ -30,6 +31,38 @@ protected:
   }
 private:
   boost::shared_ptr<RasterImage> raw_data_;
+  double scale_;
+};
+
+/****************************************************************//**
+  This is a simple adapter that multiples a underlying image by a
+  scale factor.
+*******************************************************************/
+
+class ScaleImageMultiBand : public CalcRasterMultiBand {
+public:
+  ScaleImageMultiBand(const boost::shared_ptr<RasterImageMultiBand>& Data,
+		      double Scale_factor)
+    : CalcRasterMultiBand(Data->raster_image(0), Data->number_band()), 
+      raw_data_(Data), scale_(Scale_factor) {}
+  virtual ~ScaleImageMultiBand() {}
+  virtual void print(std::ostream& Os) const
+  {
+    Os << "ScaleImageMultiBand\n"
+       << "  scale factor: " << scale_ << "\n"
+       << "  underlying raster multi_band: \n" << *raw_data_ << "\n";
+  }
+  const boost::shared_ptr<RasterImageMultiBand>& 
+  raw_data() const {return raw_data_;}
+  double scale_factor() const {return scale_;}
+protected:
+  virtual void calc(int Lstart, int Sstart) const
+  {
+    data = scale_ * raw_data_->read_double(Lstart, Sstart, data.cols(), 
+					   data.depth());
+  }
+private:
+  boost::shared_ptr<RasterImageMultiBand> raw_data_;
   double scale_;
 };
 }
--- geocal/lib/GeoCalCore/Implementation/scale_image.i
+++ geocal/lib/GeoCalCore/Implementation/scale_image.i
@@ -7,7 +7,9 @@
 #include "scale_image.h"
 %}
 %base_import(calc_raster)
+%base_import(calc_raster_multi_band)
 %geocal_shared_ptr(GeoCal::ScaleImage);
+%geocal_shared_ptr(GeoCal::ScaleImageMultiBand);
 
 namespace GeoCal {
 class ScaleImage : public CalcRaster {
@@ -15,6 +17,17 @@ public:
   ScaleImage(const boost::shared_ptr<RasterImage>& Data,
 	     double Scale_factor);
   %python_attribute(raw_data, boost::shared_ptr<RasterImage>)
+  %python_attribute(scale_factor, double)
+  %pickle_init(1, self.raw_data, self.scale_factor)
+protected:
+  virtual void calc(int Lstart, int Sstart) const;
+};
+
+class ScaleImageMultiBand : public CalcRasterMultiBand {
+public:
+  ScaleImageMultiBand(const boost::shared_ptr<RasterImageMultiBand>& Data,
+	     double Scale_factor);
+  %python_attribute(raw_data, boost::shared_ptr<RasterImageMultiBand>)
   %python_attribute(scale_factor, double)
   %pickle_init(1, self.raw_data, self.scale_factor)
 protected:

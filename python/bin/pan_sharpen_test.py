17ffa189bd0c4267db87d69cf9b324eb62bb065c
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2013-12-09 14:13:18 -0800

    Add unit test for stand alone pan_sharpen program

---
+++ python/bin/pan_sharpen_test.py
@@ -0,0 +1,41 @@
+from afids import *
+from nose.tools import *
+import subprocess
+import os
+test_data1 = os.path.dirname(__file__) + "/../unit_test_data/shiva_test_case/"
+test_data2 = os.path.dirname(__file__) + "/../unit_test_data/"
+expected_dir = os.path.dirname(__file__) + "/../unit_test_data/expected_results/pan_sharpen/"
+
+def test_pan_sharpen_map_projected():
+    '''Test the pan_sharpen PDF. This test is for already map projected data'''
+    subprocess.check_call(["pan_sharpen", 
+#    subprocess.check_call(["python", os.path.dirname(__file__) + "/pan_sharpen", 
+                           "inp=(%spost_pan_sub.img, \"%spost_b1:8.img\")" %
+                           (test_data1, test_data1),
+                           "out=ps1_b1.img", "force_rpc=n"])
+    # Add a check here
+    res = GdalMultiBand("ps1_b1:8.img")
+    expected = GdalMultiBand(expected_dir + "ps1_b1:8.img")
+    assert res.number_band == expected.number_band
+    assert res.raster_image(0).number_line == expected.raster_image(0).number_line
+    assert res.raster_image(0).number_sample == expected.raster_image(0).number_sample
+    d = res.read_double(0,0,res.raster_image(0).number_line, res.raster_image(0).number_sample)
+    dexpect = expected.read_double(0,0,expected.raster_image(0).number_line, expected.raster_image(0).number_sample)
+    assert abs(d - dexpect).max() < 0.5
+
+def test_pan_sharpen_rpc():
+    '''Test the pan_sharpen PDF. This test is for data with an RPC'''
+    subprocess.check_call(["pan_sharpen", 
+#    subprocess.check_call(["python", os.path.dirname(__file__) + "/pan_sharpen", 
+                           "inp=(%span.tif, %smul.tif)" %
+                           (test_data2, test_data2),
+                           "out=ps2_b1.img"])
+    res = GdalMultiBand("ps2_b1:8.img")
+    expected = GdalMultiBand(expected_dir + "ps2_b1:8.img")
+    assert res.number_band == expected.number_band
+    assert res.raster_image(0).number_line == expected.raster_image(0).number_line
+    assert res.raster_image(0).number_sample == expected.raster_image(0).number_sample
+    d = res.read_double(0,0,res.raster_image(0).number_line, res.raster_image(0).number_sample)
+    dexpect = expected.read_double(0,0,expected.raster_image(0).number_line, expected.raster_image(0).number_sample)
+    assert abs(d - dexpect).max() < 0.5
+    

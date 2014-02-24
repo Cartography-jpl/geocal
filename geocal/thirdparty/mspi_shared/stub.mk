ee92c69fa659244e48b50e88a54a1b65b9bcfe73
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2014-02-24 15:02:40 -0800

    Checkpoint work. This is broken, we are in the middle of sorting out stuff. But save what we have, because there is a fair chunk of work done here.

--- geocal/thirdparty/mspi_shared/stub.mk
+++
@@ -1,8 +0,0 @@
-# Retrieve subdirectory name
-local_dir := $(stubdirectory)
-
-# Load extra rules for this module (if available)
--include $(local_dir)/module.mk
-
-# Load common makefile
-include $(COMMON_MK)

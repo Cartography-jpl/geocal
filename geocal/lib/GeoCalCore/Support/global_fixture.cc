8c23094e5f75f1468755efa706430afe1aa1b3bb
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2014-02-25 15:46:26 -0800

    Start cleaning up directory structure.

--- geocal/lib/GeoCalCore/Support/global_fixture.cc
+++ geocal/lib/GeoCalCore/Support/global_fixture.cc
@@ -15,22 +15,3 @@ GlobalFixture::GlobalFixture()
   setenv("SPICEDATA", (test_data_dir() + "spice").c_str(), 1);
 }
 
-//-----------------------------------------------------------------------
-/// Directory where test data is. This already includes the trailing
-/// slash, so you can just do test_data_data() + "foo.txt" in your 
-/// unit tests.
-//-----------------------------------------------------------------------
-
-std::string GlobalFixture::test_data_dir() const
-{
-  char* srcdir = getenv("abs_top_srcdir");
-  // This should get set in set_default_value, but just in case
-  // something odd happens print an error message.
-  if(!srcdir)
-    BOOST_FAIL("To run this test, you must set the 'abs_top_srcdir' environment\n"
-               "variable to the top of the source tree. This is automatically\n"
-               "done if you are running 'make check', but you need to\n"
-
-               "running in a debugger");
-  return std::string(srcdir) + "/afids_python/geocal/unit_test_data/";
-}

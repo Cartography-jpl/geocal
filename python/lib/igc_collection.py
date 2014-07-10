2f624e06ae25cc9bc377c9a638bf7799263157d6
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2014-07-10 15:48:19 -0700

    Extend IgcArray to work with IgcCollecition as well as ImageGroundConnection

--- python/lib/igc_collection.py
+++ python/lib/igc_collection.py
@@ -124,6 +124,12 @@ class IgcArray(IgcCollection):
     everything to an array of ImageGroundConnection. You should fill in
     the attribute "igc" as a array of these values.
 
+    igc can also contain zero or more IgcCollection. We treat this like 
+    a stack of ImageGroundConnection. So if igccol1 contains 2 images, igc2
+    is a ImageGroundConnection, and igccol3 contains 3 images then the first 
+    image is the first image of igccol1, the second is the second image of 
+    igccol1, the third is igc2, and the fourth is the first image of iggcol3.
+
     The ImageGroundConnection should have the attributes "image" and
     "title" filled in. This isn't present in the underlying C++ 
     ImageGroundConnection class, but python classes such as 
@@ -169,6 +175,12 @@ class IgcArray(IgcCollection):
     @parameter_subset_mask.setter
     def parameter_subset_mask(self, val):
         self._parameter_subset_mask = val
+        pstart = 0
+        for ig in self.igc:
+            pl = len(ig.parameter)
+            if(not isinstance(ig, ImageGroundConnection)):
+                ig.parameter_subset_mask = val[pstart:(pstart + pl)]
+            pstart += pl
 
     @property
     def parameter_name(self):
@@ -181,21 +193,55 @@ class IgcArray(IgcCollection):
     @property
     def number_image(self):
         '''Number of images in this collection.'''
-        return len(self.igc)
+        res = 0
+        for ig in self.igc:
+            if(isinstance(ig, ImageGroundConnection)):
+                res += 1
+            else:
+                res += ig.number_image
+        return res
+
+    def _igc_or_coll(self, image_index):
+        '''Return ImageGroundConnection or IgcCollection for the given 
+        image_index. Also return the index into the IgcCollection, or -1'''
+        i = 0
+        if(image_index < 0):
+            raise IndexError("image_index out of range")
+        for ig in self.igc:
+            if(isinstance(ig, ImageGroundConnection)):
+                if(i == image_index):
+                    return ig, -1
+                i += 1
+            else:
+                if(image_index < i + ig.number_image):
+                    return ig, image_index - i
+                i += ig.number_image
+        raise IndexError("image_index out of range")
+
+    def _igc_or_coll_call(self, image_index, func_name, *args):
+        '''Call the given function with the given arguments for
+        the given image_index.'''
+        ig, i = self._igc_or_coll(image_index)
+        if(i < 0):
+            return getattr(ig, func_name)(*args)
+        return getattr(ig, func_name)(i, *args)
 
     def ground_coordinate(self, image_index, ic, dem = None):
         '''Return ground coordinates for the given point'''
-        return self.igc[image_index].ground_coordinate(ic, dem)
+        return self._igc_or_coll_call(image_index, "ground_coordinate", 
+                                      ic, dem)
 
     def image_coordinate(self, image_index, ground_point):
         '''Return image coordinates for the given ground point'''
-        return self.igc[image_index].image_coordinate(ground_point)
+        return self._igc_or_coll_call(image_index, "image_coordinate", 
+                                      ground_point)
 
     def image_coordinate_jac_ecr(self, image_index, ground_point):
         '''Return Jacobian for image coordinate with respect to change in 
         the ECR coordinates of the ground point. This can either be a 
         dense matrix or a scip.sparse matrix.'''
-        return self.igc[image_index].image_coordinate_jac_ecr(ground_point)
+        return self._igc_or_coll_call(image_index, "image_coordinate_jac_ecr", 
+                                      ground_point)
 
     def image_coordinate_jac_parm(self, image_index, ground_point, jac,
                                   jac_row, jac_col, line_scale = 1, 
@@ -213,37 +259,60 @@ class IgcArray(IgcCollection):
         pstart = -1
         pend = -1
         pm = self.parameter_subset_mask
-        for i, ig in enumerate(self.igc):
+        i = 0
+        for ig in self.igc:
             pl = len(ig.parameter)
             jl = pm[nparm:(nparm + pl)].count(True)
-            if(i == image_index):
+            if(isinstance(ig, ImageGroundConnection)):
+                i += 1
+            else:
+                i += ig.number_image
+            if(image_index < i):
                 pstart = jparm + jac_col
                 pmsub = pm[nparm:(nparm + pl)]
+                break
             nparm += pl
             jparm += jl
-        jac_in = self.igc[image_index].image_coordinate_jac_parm(ground_point)
-        col = pstart
-        for (i, pmval) in enumerate(pmsub):
-            if(pmval):
-                jac[jac_row, col] = jac_in[0, i] * line_scale
-                jac[jac_row + 1, col] = jac_in[1, i] * sample_scale
-                col += 1
+        ig, i = self._igc_or_coll(image_index)
+        if(i < 0):
+            jac_in = self.igc[image_index].image_coordinate_jac_parm(ground_point)
+            col = pstart
+            for (i, pmval) in enumerate(pmsub):
+                if(pmval):
+                    jac[jac_row, col] = jac_in[0, i] * line_scale
+                    jac[jac_row + 1, col] = jac_in[1, i] * sample_scale
+                    col += 1
+        else:
+            ig.image_coordinate_jac_parm(i, ground_point, jac, jac_row, pstart,
+                                         line_scale, sample_scale)
 
     def image_title(self, image_index):
         '''Title to use when displaying the given image'''
-        return self.igc[image_index].title
+        ig, i = self._igc_or_coll(image_index)
+        if(i < 0):
+            return ig.title
+        return ig.image_title(i)
 
     def image(self, image_index):
         '''Image corresponding to the given image index.'''
-        return self.igc[image_index].image
+        ig, i = self._igc_or_coll(image_index)
+        if(i < 0):
+            return ig.image
+        return ig.image(i)
 
     def dem(self, image_index):
         '''Return DEM use for given image index'''
-        return self.igc[image_index].dem
+        ig, i = self._igc_or_coll(image_index)
+        if(i < 0):
+            return ig.dem
+        return ig.dem(i)
 
     def image_ground_connection(self, image_index):
         '''Return ImageGroundConnection for the given image'''
-        return self.igc[image_index]
+        ig, i = self._igc_or_coll(image_index)
+        if(i < 0):
+            return ig
+        return ig.image_ground_connection(i)
 
     def subset(self, *indexset):
         '''Return IgcCollection that contains just the selected 

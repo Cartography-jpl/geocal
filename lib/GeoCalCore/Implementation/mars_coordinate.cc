42db7f924a13d83e8843b3bf90ab0d18635c4b6d
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2014-09-02 18:15:09 -0700

    Have basic implementation of MarsFixed in place.

--- lib/GeoCalCore/Implementation/mars_coordinate.cc
+++ lib/GeoCalCore/Implementation/mars_coordinate.cc
@@ -1,5 +1,17 @@
 #include "mars_coordinate.h"
+#include "geocal_matrix.h"
 using namespace GeoCal;
+
+// We probably want these in the class/template somehow, but for now
+// just put these here
+
+const double mars_a = 3396000.0;
+				// Equatorial radius in meters.
+const double mars_b = 3396000.0;
+				// Polar radius in meters.
+const double mars_esq = (mars_a * mars_a - mars_b * mars_b) / 
+                         (mars_a * mars_a);
+				// Eccentricity squared. From CRC.
 
 //-----------------------------------------------------------------------
 /// Create MarsFixed from GroundCoordinate
@@ -7,6 +19,16 @@ using namespace GeoCal;
 
 MarsFixed::MarsFixed(const GroundCoordinate& Gc)
 {
+  boost::shared_ptr<CartesianFixed> cf = Gc.convert_to_cf();
+  boost::shared_ptr<MarsFixed> mf = 
+    boost::dynamic_pointer_cast<MarsFixed>(cf);
+  if(!mf) {
+    Exception e;
+    e << "Cannot convert ground coordinate to MarsFixed\n"
+      << "Coordinate: " << Gc << "\n";
+    throw e;
+  }
+  position = mf->position;
 }
 
 boost::shared_ptr<CartesianInertial> 
@@ -16,18 +38,25 @@ MarsFixed::convert_to_ci(const Time& T) const
 
 double MarsFixed::height_reference_surface() const
 {
+  // We use a reference sphere instead of ellipsoid. We'll need to
+  // see if this needs to be generalized.
+  return norm(position) - mars_a;
 }
 
 double MarsFixed::min_radius_reference_surface() const
 {
+  return mars_a;
 }
 
 double MarsFixed::latitude() const
 {
+  return atan2(position[2],
+	       sqrt(position[0] * position[0] + position[1] * position[1]));
 }
 
 double MarsFixed::longitude() const
 {
+  return atan2(position[1], position[0]);
 }
 
 boost::shared_ptr<CartesianFixed>
@@ -35,9 +64,32 @@ MarsFixed::reference_surface_intersect_approximate
 (const CartesianFixedLookVector& Cl, 
  double Height_reference_surface) const
 {
+  double aph = mars_a + Height_reference_surface;
+  double bph = mars_b + Height_reference_surface;
+  boost::array<double, 3> dirci;
+  dirci[0] = Cl.look_vector[0]/ aph;
+  dirci[1] = Cl.look_vector[1]/ aph;
+  dirci[2] = Cl.look_vector[2]/ bph;
+  double t = norm(dirci);
+  dirci[0] /= t;
+  dirci[1] /= t;
+  dirci[2] /= t;
+  boost::array<double, 3> pci;
+  pci[0] = position[0] / aph;
+  pci[1] = position[1] / aph;
+  pci[2] = position[2] / bph;
+  double ddotp = dot(dirci, pci);
+  double dl = -ddotp - sqrt(ddotp * ddotp + (1 - dot(pci, pci)));
+  boost::array<double, 3> res;
+  res[0] = (pci[0] + dirci[0] * dl) * aph;
+  res[1] = (pci[1] + dirci[1] * dl) * aph;
+  res[2] = (pci[2] + dirci[2] * dl) * bph;
+  return create(res);
 }
 
 void MarsFixed::print(std::ostream& Os) const
 {
+  Os << "MarsFixed (" << position[0] << " m, " << position[1] << " m, "
+     << position[2] << "m)";
 }
 
--- lib/GeoCalCore/Implementation/mars_coordinate_test.cc
+++ lib/GeoCalCore/Implementation/mars_coordinate_test.cc
@@ -9,7 +9,18 @@ BOOST_FIXTURE_TEST_SUITE(mars_coordinate, GlobalFixture)
 BOOST_AUTO_TEST_CASE(mars_fixed)
 {
   MarsFixed mf(10, 20, 30);
-
+  BOOST_CHECK_CLOSE(mf.position[0], 10.0, 1e-8);
+  BOOST_CHECK_CLOSE(mf.position[1], 20.0, 1e-8);
+  BOOST_CHECK_CLOSE(mf.position[2], 30.0, 1e-8);
+  boost::shared_ptr<GroundCoordinate> gc = mf.convert_to_cf();
+  MarsFixed mf2(*gc);
+  BOOST_CHECK_CLOSE(mf2.position[0], 10.0, 1e-8);
+  BOOST_CHECK_CLOSE(mf2.position[1], 20.0, 1e-8);
+  BOOST_CHECK_CLOSE(mf2.position[2], 30.0, 1e-8);
+  BOOST_CHECK_CLOSE(mf.min_radius_reference_surface(), 3396000.0, 1e-8);
+  // Need some data from MarsPlanetocentric to check
+  // height_reference_surface, latitude, longitude, and 
+  // reference_surface_intersect_approximate
 }
 
 BOOST_AUTO_TEST_SUITE_END()

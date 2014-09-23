4fe0e52c83570d1280e4bca0dde6d4fc15794508
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2014-09-23 15:47:54 -0700

    Checkpoint work on JobDatabase

--- python/lib/job_database.py
+++ python/lib/job_database.py
@@ -1,6 +1,22 @@
 import os
+import pickle
 import sqlite3
+import time 
+import signal
+import subprocess
+from misc import pid_exists
 
+db_save = None
+job_id_save = None
+def _signal_handle(signum, stack):
+    db_save.execute('''
+update job_status SET status='interrupted', 
+modify_time=strftime('%Y-%m-%d %H:%M:%f', 'now'),
+pid=null,
+job_start_time=null where job_id=?
+''', (job_id_save,))
+    db_save.commit()
+    
 class JobDatabase(object):
     '''This handles a simple database used for job management, 
     checking and starting jobs. This is a very simple job management 
@@ -19,9 +35,10 @@ class JobDatabase(object):
         if(db_name is None):
             db_name = os.environ["AFIDS_JOB_STATUS_DATABASE"]
         self.db = sqlite3.connect(db_name)
+        self.db.row_factory = sqlite3.Row        
         self.db.execute('''
 CREATE TABLE IF NOT EXISTS job_status
-(job_id INTEGER PRIMARY_KEY, working_directory text not null, creation_time datetime not null, modify_time datetime not null, job_status text not null, pid INTEGER, job_start_time datetime, job_to_run BLOB)
+(job_id INTEGER PRIMARY KEY ASC, working_directory text not null, creation_time datetime not null, modify_time datetime not null, status text not null, pid INTEGER, job_start_time datetime, job_to_run BLOB)
 ''')
         
     def __del__(self):
@@ -33,3 +50,115 @@ CREATE TABLE IF NOT EXISTS job_status
             self.db.commit()
             self.db.close()
             self.db = None
+
+    def add_job(self, working_dir, job_to_run, start_if_available = True):
+        '''Submit a job to run. You supply the working directory (can
+        be relative if desired, we call os.path.abspath on this), and
+        a job to run. The job should be an array like what can be
+        passed to subprocess.check_call.
+
+        Note that by default we check and see if the job can be run, and
+        if so the job is started (asynchronously, so we don't wait for
+        it to finish). You can turn is off is desired by passing
+        'start_if_available' as False.
+
+        This return the job id of the submitted job.'''
+        cur = self.db.execute('''
+INSERT INTO job_status VALUES(null, ?, strftime('%Y-%m-%d %H:%M:%f', 'now'),
+strftime('%Y-%m-%d %H:%M:%f', 'now'), 'queued', null, null, ?)''', 
+                        (os.path.abspath(working_dir), 
+                         pickle.dumps(job_to_run)))
+        self.db.commit()
+        return cur.lastrowid
+
+    def __getitem__(self, job_id):
+        '''Return information about a job with the given job_id.'''
+        r = self.db.execute("select * from job_status where job_id=?",
+                            (job_id,)).fetchone()
+        if(r is None):
+            raise KeyError(job_id)
+        return r
+
+    def keys(self):
+        '''Return the list of jobs ids.'''
+        return [row[0] for row in
+                self.db.execute("select job_id from job_status")]
+
+    def __delitem__(self, job_id):
+        self.db.execute("DELETE FROM job_status WHERE job_id=?", [job_id])
+
+    def set_status(self, job_id, status):
+        '''Set a job id to the given status.'''
+        if(status == "running"):
+            self.db.execute('''
+update job_status SET status='running', 
+modify_time=strftime('%Y-%m-%d %H:%M:%f', 'now'),
+pid=?,
+job_start_time=strftime('%Y-%m-%d %H:%M:%f', 'now') where job_id=?
+''', (os.getpid(), job_id))
+        else:
+            self.db.execute('''
+update job_status SET status=?, 
+modify_time=strftime('%Y-%m-%d %H:%M:%f', 'now'),
+pid=null,
+job_start_time=null where job_id=?
+''', (status, job_id))
+
+        self.db.commit()
+        
+    def check_and_run(self):
+        '''This checks if:
+
+        1. There is currently no running job
+        2. There is a job in the queue read to run.
+
+        If so, this starts the job, and waits for it to finish before
+        returning. We then update the state of the job to:
+
+        1. "success"
+        2. "failure" (error occurred)
+        3. "interrupted" (if a signal occurred during the run)
+
+        Return true if a job was run, false otherwise.
+        '''
+        r = self.db.execute('''
+select job_id, pid from job_status where status = 'running'
+''').fetchone()
+        if(r is not None):
+            if(pid_exists(r["pid"])):
+                # Job running, so don't start another one
+                return False
+        # We may have had the job disappear (e.g., a SIGKILL occurred).
+            self.db.execute('''
+update job_status SET status='interrupted', 
+modify_time=strftime('%Y-%m-%d %H:%M:%f', 'now'),
+pid=null,
+job_start_time=null where job_id=?
+''', (r["job_id"],))
+            self.db.commit()
+        r = self.db.execute('''
+select job_id, job_to_run from job_status where status = 'queued' 
+order by job_id
+''').fetchone()
+        if(r is None):
+            return False
+        old_signal = {}
+        global db_save
+        global job_id_save
+        db_save = self.db
+        job_id_save = r["job_id"]
+        siglist = [signal.SIGHUP, signal.SIGINT, signal.SIGQUIT, signal.SIGTERM]
+        try:
+            for s in siglist:
+                old_signal[s] = signal.signal(s, _signal_handle)
+            self.set_status(r["job_id"], "running")
+            subprocess.check_output(pickle.loads(r["job_to_run"]),
+                                    stderr=subprocess.STDOUT)
+            self.set_status(r["job_id"], "success")
+        except subprocess.CalledProcessError:
+            self.set_status(r["job_id"], "failure")
+        finally:
+            for s in siglist:
+                if(s in old_signal):
+                    signal.signal(s, old_signal[s])
+        return True
--- python/lib/job_database_test.py
+++ python/lib/job_database_test.py
@@ -1,5 +1,42 @@
 from nose.tools import *
 from job_database import *
+from misc import *
 
 def test_job_database():
     jdb = JobDatabase("test.db")
+    for jid in jdb.keys():
+        del jdb[jid]
+    jid = jdb.add_job("jobs_database_dir", ["ls"])
+    assert jdb.check_and_run()
+    assert jdb[jid]["status"] == "success"
+
+def test_handle_running_job():
+    jdb = JobDatabase("test.db")
+    for jid in jdb.keys():
+        del jdb[jid]
+    jid_running = jdb.add_job("jobs_database_dir", ["ls"])
+    jdb.set_status(jid_running, "running")
+    jid_could_run = jdb.add_job("jobs_database_dir", ["ls"])
+    assert not jdb.check_and_run()
+
+def test_handle_interrupted_job():
+    jdb = JobDatabase("test.db")
+    for jid in jdb.keys():
+        del jdb[jid]
+    jid_running = jdb.add_job("jobs_database_dir", ["ls"])
+    jdb.set_status(jid_running, "running")
+    # Fake a PID no longer running
+    jdb.db.execute("update job_status set pid=-1 where job_id=?", (jid_running,))
+    jdb.db.commit()
+    jid_could_run = jdb.add_job("jobs_database_dir", ["ls"])
+    assert jdb.check_and_run()
+    assert jdb[jid_running]["status"] == "interrupted"
+    assert jdb[jid_could_run]["status"] == "success"
+
+def test_handle_failed_job():
+    jdb = JobDatabase("test.db")
+    for jid in jdb.keys():
+        del jdb[jid]
+    jid = jdb.add_job("jobs_database_dir", ["ls", "--bad-argument"])
+    assert jdb.check_and_run()
+    assert jdb[jid]["status"] == "failure"

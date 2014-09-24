30c2610bece0038c96655275541100feeeff8478
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2014-09-24 13:13:13 -0700

    Add handling so unit test doesn't fail if we don't have MSPI_SHARED support.

--- lib/Mspi/air_mspi_igc_test.cc
+++ lib/Mspi/air_mspi_igc_test.cc
@@ -8,6 +8,7 @@ BOOST_FIXTURE_TEST_SUITE(air_mspi_igc, GlobalFixture)
 
 BOOST_AUTO_TEST_CASE(chain_test)
 {
+#ifdef HAVE_MSPI_SHARED
   // Skip test if data isn't available
   if(!boost::filesystem::is_directory("/home/smyth/Local/MSPI-Ground/chain_test/input"))
     return;
@@ -19,6 +20,9 @@ BOOST_AUTO_TEST_CASE(chain_test)
 	"/home/smyth/Local/MSPI-Ground/chain_test/3.l1b1.hdf5",
 	1, 
 	"/home/smyth/Local/MSPI-Ground");
+#else
+  BOOST_WARN_MESSAGE(false, "Skipping AirMspiIgc test, because we haven't build ith MSPI_SHARED support");
+#endif
 }
 
 BOOST_AUTO_TEST_SUITE_END()

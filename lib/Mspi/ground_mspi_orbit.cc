8289255a5b8050c08a68dbedd997e90c9f84cf3c
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2014-10-28 14:46:25 -0700

    Have GroundMspiOrbit working with TimeWithDerivative.

--- lib/Mspi/ground_mspi_orbit.cc
+++ lib/Mspi/ground_mspi_orbit.cc
@@ -22,6 +22,31 @@ GroundMspiOrbit::orbit_data(Time T) const
     (new QuaternionOrbitData(T, pos->convert_to_cf(), vel, sc_to_cf));
 }
 
+boost::shared_ptr<OrbitData> 
+GroundMspiOrbit::orbit_data(const TimeWithDerivative& T) const
+{
+  // Space craft to local north
+  AutoDerivative<double> zenith = (start_elevation_angle_ + 
+		   (T - tstart) * rotation_rate_);
+  boost::math::quaternion<AutoDerivative<double> > sc_to_ln = 
+    quat_rot("zy", 
+	     AutoDerivative<double>((90 - azimuth_) * Constant::deg_to_rad),
+	     -zenith * Constant::deg_to_rad);
+  boost::math::quaternion<double> ln_to_cf = LnLookVector::enu_to_cf(*pos);
+  boost::math::quaternion<AutoDerivative<double> >  
+    sc_to_cf = ln_to_cf * sc_to_ln;
+  boost::array<AutoDerivative<double>, 3> vel = {{AutoDerivative<double>(0),
+						  AutoDerivative<double>(0),
+						  AutoDerivative<double>(0)}};
+  boost::shared_ptr<CartesianFixed> p = pos->convert_to_cf();
+  boost::array<AutoDerivative<double>, 3> pos_der = 
+    {{AutoDerivative<double>(p->position[0]),
+      AutoDerivative<double>(p->position[1]),
+      AutoDerivative<double>(p->position[2])}};
+  return boost::shared_ptr<OrbitData>
+    (new QuaternionOrbitData(T, p, pos_der, vel, sc_to_cf));
+}
+
 void GroundMspiOrbit::print(std::ostream& Os) const {
   Os << "GroundMspiOrbit\n"
      << "  Start time: " << tstart << "\n"

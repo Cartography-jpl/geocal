3d7e451da0205204fd47914d2e0b437c1fb370fa
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2014-11-04 13:40:18 -0800

    Have igc_offset_correction_test working

--- python/lib/igc_offset_correction_test.py
+++ python/lib/igc_offset_correction_test.py
@@ -25,7 +25,6 @@ img2 = VicarLiteRasterImage(test_data + "10MAY21-2.img")
 img3 = VicarLiteRasterImage(test_data + "10MAY21-3.img")
 
 def test_igc():
-    raise SkipTest # Skip for now
     try:
         # Depending on the options used when building, this class might
         # not be available. If not, then just skip this test.
@@ -63,7 +62,6 @@ def test_igc():
     assert_almost_equal(igc_coll2.image_coordinate(0, gp).sample, ic.sample, 3)
 
 def test_igc_jac():
-    raise SkipTest # Skip for now
     try:
         # Depending on the options used when building, this class might
         # not be available. If not, then just skip this test.

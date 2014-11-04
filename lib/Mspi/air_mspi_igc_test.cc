0ab5fa2bf83a198fa9932709160bb67e159a4c9f
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2014-11-04 11:15:57 -0800

    Add test data in place to run AirMspiIgc test w/o depending on a separate MSPI-Ground directory.

--- lib/Mspi/air_mspi_igc_test.cc
+++ lib/Mspi/air_mspi_igc_test.cc
@@ -9,17 +9,12 @@ BOOST_FIXTURE_TEST_SUITE(air_mspi_igc, GlobalFixture)
 BOOST_AUTO_TEST_CASE(chain_test)
 {
 #ifdef HAVE_MSPI_SHARED
-  // Skip test if data isn't available
-  if(!boost::filesystem::is_directory("/home/smyth/Local/MSPI-Ground/chain_test/input"))
-    return;
-  // Grab data from Mike's chain test. We'll either modify this or
-  // copy it, but for now grab from Mike's area
   AirMspiIgc 
-    igc("/home/smyth/Local/MSPI-Ground/chain_test/input/3.master.config", 
-	"/home/smyth/Local/MSPI-Ground/chain_test/input/3.nav_gimbal.hdf4",
-	"/home/smyth/Local/MSPI-Ground/chain_test/3.l1b1.hdf5",
+    igc(test_data_dir() + "/mspi/3.master.config", 
+	test_data_dir() + "/mspi/3.nav_gimbal.hdf4",
+	test_data_dir() + "/mspi/3.l1b1.hdf5",
 	1, 
-	"/home/smyth/Local/MSPI-Ground");
+	test_data_dir() + "/mspi");
 #else
   BOOST_WARN_MESSAGE(false, "Skipping AirMspiIgc test, because we haven't build ith MSPI_SHARED support");
 #endif

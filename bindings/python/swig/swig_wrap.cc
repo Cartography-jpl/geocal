621e9063ad56afec9fe4dad37a1f6e52d421630d
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2014-12-19 15:35:16 -0800

    Add swig for AirMspiTimeTable.

--- bindings/python/swig/swig_wrap.cc
+++ bindings/python/swig/swig_wrap.cc
@@ -134,6 +134,7 @@ extern "C" {
   void init_ground_mspi_orbit(void);
   void init_ground_mspi_igc(void);
   void init_usgs_dem(void);
+  void init_air_mspi_time_table(void);
   void init_air_mspi_igc(void);
 #ifdef HAVE_CARTO
   void init_eci_tod_burl(void);
@@ -359,6 +360,7 @@ void init_swig_wrap(void)
   init_extension_module(package, "_ground_mspi_orbit", init_ground_mspi_orbit);
   init_extension_module(package, "_ground_mspi_igc", init_ground_mspi_igc);
   init_extension_module(package, "_usgs_dem", init_usgs_dem);
+  init_extension_module(package, "_air_mspi_time_table", init_air_mspi_time_table);
   init_extension_module(package, "_air_mspi_igc", init_air_mspi_igc);
 #ifdef HAVE_CARTO
   init_extension_module(package, "_eci_tod_burl", init_eci_tod_burl);

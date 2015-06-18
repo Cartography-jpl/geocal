3d0e29f6819ca06c312035c2c7d6dbfe1bc9a1f2
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2015-06-18 10:15:23 -0700

    Add serialization of Covariance (slowly working through all the code)

--- lib/GeoCalCore/Support/covariance.cc
+++ lib/GeoCalCore/Support/covariance.cc
@@ -1,6 +1,19 @@
 #include "covariance.h"
-
+#include "geocal_serialize_support.h"
 using namespace GeoCal;
+
+#ifdef GEOCAL_HAVE_BOOST_SERIALIZATION
+
+template<class Archive> 
+void Covariance::serialize(Archive& ar, const unsigned int version)
+{
+  GEOCAL_GENERIC_BASE(Covariance)
+  ar & GEOCAL_NVP(sum1) & GEOCAL_NVP(sum2) & GEOCAL_NVP(sum_product)
+    & GEOCAL_NVP_(count);
+}
+
+GEOCAL_IMPLEMENT(Covariance);
+#endif
 
 //-----------------------------------------------------------------------
 /// Print to stream
--- lib/GeoCalCore/Support/covariance_test.cc
+++ lib/GeoCalCore/Support/covariance_test.cc
@@ -27,4 +27,31 @@ BOOST_AUTO_TEST_CASE(basic_test)
   BOOST_CHECK_CLOSE(c.covariance(), -0.01875, 1e-8);
 }
 
+BOOST_AUTO_TEST_CASE(serialize)
+{
+  if(!have_serialize_supported())
+    return;
+  boost::shared_ptr<Covariance> c(new Covariance());
+  c->add(1.0, 2.05);
+  c->add(1.1, 2.2);
+  c->add(1.2, 1.9);
+  c->add(1.3, 1.8);
+  c->add(0.9, 2.1);
+  BOOST_CHECK_EQUAL(c->count(), 5);
+  BOOST_CHECK_CLOSE(c->mean1(), 1.1, 1e-8);
+  BOOST_CHECK_CLOSE(c->mean2(), 2.01, 1e-8);
+  BOOST_CHECK_CLOSE(c->covariance(), -0.01875, 1e-8);
+  std::string d = serialize_write_string(c);
+  if(false)
+    // Can dump to screen, if we want to see the text
+    std::cerr << d;
+  boost::shared_ptr<Covariance> cr =
+    serialize_read_string<Covariance>(d);
+  BOOST_CHECK_EQUAL(cr->count(), 5);
+  BOOST_CHECK_CLOSE(cr->mean1(), 1.1, 1e-8);
+  BOOST_CHECK_CLOSE(cr->mean2(), 2.01, 1e-8);
+  BOOST_CHECK_CLOSE(cr->covariance(), -0.01875, 1e-8);
+}
+
+
 BOOST_AUTO_TEST_SUITE_END()

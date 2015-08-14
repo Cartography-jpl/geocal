56708f6354979512d9d750117aaa9d893e1d5dee
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2015-08-14 16:04:50 -0700

    Add a search path for temporary MSPI files, rather than just a single directory. Also add a compile time option to print out diagnostic messages about what we get from the temporary directories.

--- lib/Mspi/air_mspi_file.cc
+++ lib/Mspi/air_mspi_file.cc
@@ -2,9 +2,14 @@
 // This won't be needed once version 3 becomes the default for boost filesystem
 #define BOOST_FILESYSTEM_VERSION 3
 #include <boost/filesystem.hpp>
+#include <boost/algorithm/string.hpp>
+#include <boost/foreach.hpp>
 #include <cstdlib>
 
 using namespace GeoCal;
+// Define to print out diagnostic messages to make sure we are reading
+// cached data where we expect to.
+//#define AIR_MSPI_DIAGNOSTIC
 
 //-----------------------------------------------------------------------
 /// AirMSPI uses a file caching mechanism to speed up the processing
@@ -19,14 +24,33 @@ using namespace GeoCal;
 std::string GeoCal::air_mspi_true_file_name(const std::string& Fname)
 {
   std::string res = Fname;
+  bool changed = false;
   if(getenv("MSPI_TEMP_DIR")) {
     boost::filesystem::path p(Fname);
     std::string bname = boost::filesystem::basename(p);
-    boost::filesystem::path p2(std::string(getenv("MSPI_TEMP_DIR")));
-    p2 /= p.filename();
-    if(boost::filesystem::exists(p2)) {
-      res = p2.string();
+    std::string t = getenv("MSPI_TEMP_DIR");
+    std::vector<std::string> sv;
+    boost::split(sv, t, boost::is_any_of(":"), boost::token_compress_on);
+    BOOST_FOREACH(const std::string& s, sv) {
+      boost::filesystem::path p2(s);
+      p2 /= p.filename();
+      if(boost::filesystem::exists(p2)) {
+	res = p2.string();
+	changed = true;
+	break;
+      }
     }
   }
+#ifdef  AIR_MSPI_DIAGNOSTIC
+  if(getenv("MSPI_TEMP_DIR")) {
+    std::cout << "MSPI_TEMP_DIR: " << std::string(getenv("MSPI_TEMP_DIR")) << "\n";
+  } else {
+    std::cout << "MSPI_TEMP_DIR is not defined\n";
+  }
+  if(changed)
+    std::cout << "Mapped file " << Fname << " to " << res << "\n";
+  else
+    std::cout << "Did not map file " << Fname << " to a cached version.\n";
+#endif
   return res;
 }

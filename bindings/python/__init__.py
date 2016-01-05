c19f33acd0dc0d93c727c18ac92a0b1ef5ff88db
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2016-01-05 14:14:34 -0800

    Continue working to get python 3 swig working. Can load geocal_swig (but not geocal). Not sure if we broke anything with python 2, we'll test everything out shortly.

--- bindings/python/__init__.py
+++ bindings/python/__init__.py
@@ -5,6 +5,12 @@ import os
 import glob
 
 from geocal_swig._swig_wrap import *
+from sys import version_info
 
 for i in glob.glob(os.path.dirname(__file__) + "/*.py"):
-    exec('from ' + os.path.basename(i).split('.')[0] + ' import *')
+    # Use relative import if we are using python 3
+    if version_info >= (3, 0, 0):
+        exec('from .' + os.path.basename(i).split('.')[0] + ' import *')
+    else:
+        exec('from ' + os.path.basename(i).split('.')[0] + ' import *')
+        
--- python/init/__init__.py
+++ python/init/__init__.py
@@ -7,10 +7,20 @@ from geocal_swig import *
 
 # Make sure we can safely import matplotlib without getting an error
 # (see this module for details on this)
-import safe_matplotlib_import
+from sys import version_info
+# Use relative import if we are using python 3
+if version_info >= (3, 0, 0):
+    from .safe_matplotlib_import import *
+else:
+    import safe_matplotlib_import
+
 
 for i in glob.glob(os.path.dirname(__file__) + "/*.py"):
     mname = os.path.basename(i).split('.')[0]
     # Don't load ipython, which is ipython magic extensions
     if(not mname == 'ipython'):
-        exec("from %s import *" % mname)
+        # Use relative import if we are using python 3
+        if version_info >= (3, 0, 0):
+            exec("from .%s import *" % mname)
+        else:
+            exec("from %s import *" % mname)

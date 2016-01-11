16dc5ee5fa28ebef21db8f057a1b38148773de7b
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2016-01-11 14:29:26 -0800

    Some makefile and __init__.py updates to support python 3. Note that we still don't have python 3 working, however python 2 is still testing cleanly. Checkpoint this work.

--- python/init/__init__.py
+++ python/init/__init__.py
@@ -1,16 +1,16 @@
 # Just import any files we find in this directory, rather than listing
 # everything
 
+from __future__ import absolute_import
 import os
 import glob
 from geocal_swig import *
 
-# Make sure we can safely import matplotlib without getting an error
-# (see this module for details on this)
-import safe_matplotlib_import
+from .safe_matplotlib_import import *
+
 
 for i in glob.glob(os.path.dirname(__file__) + "/*.py"):
     mname = os.path.basename(i).split('.')[0]
     # Don't load ipython, which is ipython magic extensions
     if(not mname == 'ipython'):
-        exec("from %s import *" % mname)
+        exec("from .%s import *" % mname)

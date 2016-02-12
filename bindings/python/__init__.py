8f84eaa8bd3ad31353f8214a2dc443175add2fae
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2016-02-11 17:23:53 -0800

    Have a version of __init__ that doesn't use exec. This is a bit faster, although the code is still a bit slow.

--- bindings/python/__init__.py
+++ bindings/python/__init__.py
@@ -4,8 +4,10 @@
 from __future__ import absolute_import
 import os
 import glob
+import importlib
 
 from geocal_swig._swig_wrap import *
 
 for i in glob.glob(os.path.dirname(__file__) + "/*.py"):
-    exec('from .' + os.path.basename(i).split('.')[0] + ' import *')
+    mod = importlib.import_module(".%s" % os.path.basename(i).split('.')[0], "geocal_swig")
+    locals().update(mod.__dict__)
--- python/init/__init__.py
+++ python/init/__init__.py
@@ -5,6 +5,7 @@ from __future__ import absolute_import
 import os
 import glob
 from geocal_swig import *
+import importlib
 
 from .safe_matplotlib_import import *
 
@@ -12,4 +13,5 @@ for i in glob.glob(os.path.dirname(__file__) + "/*.py"):
     mname = os.path.basename(i).split('.')[0]
     # Don't load ipython, which is ipython magic extensions
     if(not mname == 'ipython'):
-        exec("from .%s import *" % mname)
+        mod = importlib.import_module(".%s" % mname, "geocal")
+        locals().update(mod.__dict__)

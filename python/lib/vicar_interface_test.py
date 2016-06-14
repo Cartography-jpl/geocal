8a5a886265f2aa0c4d836061306179af7376a38d
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2016-06-14 14:42:53 -0700

    Fix for the geocal variation tests.

--- python/lib/vicar_interface_test.py
+++ python/lib/vicar_interface_test.py
@@ -2,6 +2,7 @@ from geocal.vicar_interface import *
 from test_support import *
 
 @require_vicarb
+@require_vicar
 def test_vicar_to_numpy(isolated_dir):
     '''Test vicar_to_numpy.'''
     out = vicar_to_numpy("gen out NL=15 NS=15 IVAL=90")

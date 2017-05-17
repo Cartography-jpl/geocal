00ec7af671d063ccce85284ff8831b41a19444e9
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2017-05-17 15:06:34 -0700

    Add SpiceKernelByTime

--- python/lib/pds_label.py
+++ python/lib/pds_label.py
@@ -2,6 +2,8 @@ from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
 import re
+import bisect
+from geocal_swig import *
 
 def pds_label_text(fname):
     '''While GDAL can read PDS format, it doesn't read most of the metadata.
@@ -34,3 +36,61 @@ def pds_label(fname):
                 v = m.group(1)
             res[k] = v
     return res
+
+class SpiceKernelByTime(object):
+    '''In some cases, we have a range of SPICE kernels with one for a range
+    of times. These kernels have PDS labels associated with them that give
+    the time ranges. So for example the mro spk kernels.
+
+    This class handles parsing all the PDS labels, setting up the mapping,
+    and then loading the right kernel for a particular time.
+
+    This class can be saved (e.g., use json or pickling pickling through 
+    write_shelve) so we don't need to parse the directory every time.
+    '''
+    def __init__(self, lbl_file_list):
+        '''Parse each PDS label file. Note that we assume the spice kernel is
+        in the same directory as each PDS label.
+
+        We read the PDS labels START_TIME, STOP_TIME and PRODUCT_ID.
+
+        Often the label list can be created by a glob, e.g. 
+        glob.glob("/raid26/mars_kernels/mro_kernels/spk/mro_*_ssd_mro95a.lbl")
+        '''
+        self.data = []
+        for f in lbl_file_list:
+            d = pds_label(f)
+            self.data.append((Time.parse_time(d["START_TIME"]).et,
+                              Time.parse_time(d["STOP_TIME"]).et,
+                              os.path.abspath(os.path.dirname(f) + "/" +
+                                              d["PRODUCT_ID"])))
+        # Sort by start time
+        self.data.sort(key=lambda v : v[0])
+        self.tstart_et = [v[0] for v in self.data]
+        self.tend_et = [v[1] for v in self.data]
+        self.kernel_file = [v[2] for v in self.data]
+
+    def __getstate__(self):
+        return { "data" : self.data }
+
+    def __setstate__(self, d):
+        self.data = d["data"]
+        self.tstart_et = [v[0] for v in self.data]
+        self.tend_et = [v[1] for v in self.data]
+        self.kernel_file = [v[2] for v in self.data]
+
+    def kernel(self, tm):
+        '''Return the kernel covering the given time. If nothing 
+        matches, we raise a ValueError.'''
+        # Find largest value less than or equal to tm
+        tm_et = tm.et
+        i = bisect.bisect_left(self.tstart_et, tm_et)
+        if not i:
+            raise ValueError
+        # Check that this matches the upper range (since there may be
+        # gaps in the data)
+        if(tm_et > self.tend_et[i-1]):
+            raise ValueError
+        return self.kernel_file[i-1]
+    
+        
--- python/lib/pds_label_test.py
+++ python/lib/pds_label_test.py
@@ -1,18 +1,28 @@
 from geocal.pds_label import *
 from test_support import *
 import os
+import glob
 
 def test_pds_label_text():
     if(not os.path.exists("/raid26/tllogan/mars/mawrth_vallis/source/unmapped/P16_007388_2049_XI_24N020W.IMG")):
-        # Skip we don't have test data available
+        # Skip if we don't have test data available
         raise SkipTest
     txt = pds_label_text("/raid26/tllogan/mars/mawrth_vallis/source/unmapped/P16_007388_2049_XI_24N020W.IMG")
     print(txt)
 
 def test_pds_label():
     if(not os.path.exists("/raid26/tllogan/mars/mawrth_vallis/source/unmapped/P16_007388_2049_XI_24N020W.IMG")):
-        # Skip we don't have test data available
+        # Skip if we don't have test data available
         raise SkipTest
     d = pds_label("/raid26/tllogan/mars/mawrth_vallis/source/unmapped/P16_007388_2049_XI_24N020W.IMG")
     print(d)
+
+def test_spice_kernel_by_time():
+    bdir = "/raid26/mars_kernels/mro_kernels/spk"
+    if(not os.path.exists("/raid26/mars_kernels/mro_kernels/spk")):
+        # Skip if we don't have test data available
+        raise SkipTest
+    kdata = SpiceKernelByTime(glob.glob(bdir + "/mro_*_ssd_mro110c.lbl"))
+    tstart = Time.parse_time("2008-02-23T03:36:10.894Z")
+    assert kdata.kernel(tstart) == "/raid26/mars_kernels/mro_kernels/spk/mro_psp6_ssd_mro110c.bsp"
     

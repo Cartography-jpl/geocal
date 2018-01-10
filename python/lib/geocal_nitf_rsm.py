69c4077a21879508e94dc1bf4553b40e7cb138a7
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2018-01-10 13:31:45 -0800

    Fix a python 2.7 issue with a missing print_function import

--- python/lib/geocal_nitf_rsm.py
+++ python/lib/geocal_nitf_rsm.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 from geocal_swig import Rsm, RsmId, RsmMultiSection, RsmRationalPolynomial, RsmGrid, RsmRpPlusGrid
 try:
     from pynitf import create_nitf_tre_structure, NitfSegmentHook, NitfFile, TreRSMIDA, \

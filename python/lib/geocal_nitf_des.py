0029fa583c38aa96351845557bcb40eaec1bb833
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2018-12-17 14:41:25 -0800

    Have orbit_des attached to NitfImageSegment.

--- python/lib/geocal_nitf_des.py
+++ python/lib/geocal_nitf_des.py
@@ -1,7 +1,7 @@
 from geocal_swig import (PosCsephb, AttCsattb, OrbitDes)
 try:
     from pynitf import (DesCSEPHB, DesCSATTB, create_nitf_des_structure,
-                        register_des_class)
+                        register_des_class, NitfImageSegment)
     have_pynitf = True
 except ImportError:
     # Ok if we don't have pynitf, we just can't execute this code
@@ -46,6 +46,45 @@ This should be used to set and read the DES values.
     # gets used to read the data instead of DesCSATTB
 
     register_des_class(DesCSATTB_geocal, priority_order=-1)
+
+    # Have search to find the pos_csephb and att_csattb that goes with a
+    # image segment. We may extend this.
+    def _pos_csephb(iseg):
+        lv = iseg.idlvl
+        f = iseg.nitf_file
+        if f is None:
+            raise RuntimeError("Need to have nitf_file set for image segment level %d" % lv)
+        possible = [d for d in f.des_segment if
+                    d.subheader.desid == "CSEPHB" and
+                    lv in d.des.user_subheader.aisdlvl]
+        if(len(possible) == 0):
+            return None
+        if(len(possible) > 1):
+            raise RuntimeError("Found more than one possible pos_csephb for image esegment level %d" % lv)
+        return possible[0].des.pos_csephb
+
+    def _att_csattb(iseg):
+        lv = iseg.idlvl
+        f = iseg.nitf_file
+        if f is None:
+            raise RuntimeError("Need to have nitf_file set for image segment level %d" % lv)
+        possible = [d for d in f.des_segment if
+                    d.subheader.desid == "CSATTB" and
+                    lv in d.des.user_subheader.aisdlvl]
+        if(len(possible) == 0):
+            return None
+        if(len(possible) > 1):
+            raise RuntimeError("Found more than one possible pos_csephb for image esegment level %d" % lv)
+        return possible[0].des.att_csattb
+
+    def _orbit_des(iseg):
+        if(iseg.pos_csephb is None or iseg.att_csattb is None):
+            return None
+        return OrbitDes(iseg.pos_csephb, iseg.att_csattb)
+    
+    NitfImageSegment.pos_csephb = property(_pos_csephb)
+    NitfImageSegment.att_csattb = property(_att_csattb)
+    NitfImageSegment.orbit_des = property(_orbit_des)
     
 if(have_pynitf):
     __all__ = ["DesCSEPHB_geocal","DesCSATTB_geocal",]

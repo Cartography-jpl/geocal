55f9e4208af567078f1b0a1d546e0fccee8f628f
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2019-04-23 14:54:58 -0700

    Have a sample sensrb working with MSP. We can now try to figure out how to get our own sensrb working.

--- python/lib/geocal_nitf_sensrb_test.py
+++ python/lib/geocal_nitf_sensrb_test.py
@@ -8,6 +8,7 @@ from geocal_swig import (Time, KeplerOrbit, SimpleCamera,
                          SensrbCamera)
 from geocal.geocal_nitf_sensrb import *
 import numpy as np
+import six
 
 def create_image_seg(f):
     img = pynitf.NitfImageWriteNumpy(9, 10, np.uint8)
@@ -133,4 +134,53 @@ def test_sensrb_msp(isolated_dir):
             p1 = igc_msp.ground_coordinate_approx_height(ic, h)
             p2 = rsm.ground_coordinate_approx_height(ic, h)
             assert(geocal_swig.distance(p1, p2) < 0.01)
+
+
+@require_msp    
+@require_pynitf
+def test_sensrb_sample_msp(isolated_dir):
+    '''Use the sample SENSRB TRE make sure MSP can read it. This might go away
+    once we have our own version of writing SENSRB, but for now we'll use
+    this to have something clean to work with.
+
+    This comes from the
+    http://www.gwg.nga.mil/ntb/baseline/software/demo.html and is the file
+    SB_Seattle_WithUncertainties.txt. This doesn't have all the sections needed
+    by the MSP library, so we have to add a few. Also, the MSP library seems
+    to require Geocentric coordinates rather than lat/lon so we need to convert
+    that.'''
+    f = pynitf.NitfFile()
+    create_image_seg(f)
+    fh = six.BytesIO(b"SENSRB01804YACESHY                                                   Super Hornet                                             Airborne  1WGS84GHAESIDEG1997040500000000000000199704050000000000000000------------------YVIS                 00001024000010240000000200000002000003.5----------------NNN000000000000----------------47.58288058-122.328413900000000360000000000000000000000000Y1-000000017-000000180000000000N----------------------------NNY000000100-0000005000000000004Image Center             001000005120000051247.5951277-0122.3316513.106--------Image Footprint          004000010240000000147.5941277-0122.3326513.106--------000000010000000147.5961277-0122.3326513.106--------000000010000102447.5961277-0122.3306513.106--------000010240000102447.5941277-0122.3306513.106--------Ground Area              004000008760000021247.5944277-0122.3316513.106--------000005180000050147.5951277-0122.3321513.106--------000007750000078947.5958277-0122.3316513.106--------000004940000051247.5951277-0122.3311513.106--------Ground Points            003000008000000011547.5948277-0122.3318513.106--------000004000000050347.5954277-0122.3318513.106--------000008000000096247.5954277-0122.3313513.106--------000001911e2.1     -----------000000001011f2.1     -----------000000001011e2.2     -----------000000002011f2.2     -----------000000002011e2.3     -----------000000001011f2.3     -----------000000001011e2.4     -----------000000002011f2.4     -----------000000002011e1.1     -----------000000000511f1.1     -----------000000001011e1.1     11f1.1     000000000011e3.1     -----------000000001011f3.1     -----------000000001011e3.2     -----------000000001011f3.2     -----------000000001011e3.3     -----------000000001011f3.3     -----------000000001011e3.4     -----------000000002011f3.4     -----------0000000020000")
+    t = pynitf.TreSENSRB()
+    t.read_from_file(fh)
+
+    # Should go through and figure out what we actually need to modify
+    t.calibrated = "Y"
+    t.sensor_calibration_data = "Y"
+    t.calibration_unit = "px"
+    t.principal_point_offset_x = 0
+    t.principal_point_offset_y = 0
+    t.image_formation_data = "Y"
+    t.method = "Single Frame"
+    t.mode = "004"
+    t.row_count = 1024
+    t.column_count = 1024
+    t.row_set = 1024
+    t.column_set = 1024
+    t.row_rate = 0.001
+    t.column_rate = 0.001
+    t.first_pixel_row = 1
+    t.first_pixel_column = 1
+    t.transform_params = 0
+    # Convert to ECR coordinates, MSP doesn't work with lat/lon
+    pt = Ecr(Geodetic(t.latitude_or_x, t.longitude_or_y, t.altitude_or_z))
+    t.geodetic_type = "C"
+    t.latitude_or_x, t.longitude_or_y, t.altitude_or_z = pt.position
+    
+    f.image_segment[0].tre_list.append(t)
+    f.write("sensrb_test.ntf")
+    igc_msp = IgcMsp("sensrb_test.ntf", SimpleDem(), "SENSRB_FrameProfile",
+                     "SENSRB_FrameProfile")
+    print(Geodetic(igc_msp.ground_coordinate(ImageCoordinate(10,10))))
             

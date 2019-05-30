2644d01c444a25a64d5ff8a54fb50ca059668851
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2019-05-30 12:40:17 -0700

    Continue cleaning up SWIG code.

--- swig_rules/__init__.py
+++ swig_rules/__init__.py
@@ -5,7 +5,7 @@ from __future__ import absolute_import
 import os as _os
 import glob as _glob
 
-from geocal_swig._swig_wrap import *
+from ._swig_wrap import *
 
 for _i in _glob.glob(_os.path.dirname(__file__) + "/*.py"):
     exec('from .' + _os.path.basename(_i).split('.')[0] + ' import *')

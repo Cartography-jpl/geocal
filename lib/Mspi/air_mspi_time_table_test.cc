3fa968c650ef283baa9fb2bd6cb7eb760fb3b8df
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2021-04-14 14:24:59 -0700

    Update expected results of time table.

--- lib/Mspi/air_mspi_time_table_test.cc
+++ lib/Mspi/air_mspi_time_table_test.cc
@@ -16,7 +16,7 @@ BOOST_AUTO_TEST_CASE(chain_test)
   BOOST_CHECK_CLOSE(fc.line, 0, 1e-4);
   BOOST_CHECK_CLOSE(fc.sample, 100, 1e-4);
   tt.time(ImageCoordinate(1, 100), t2, fc);
-  BOOST_CHECK_CLOSE(t2 - t, 0.039891, 1e-4);
+  BOOST_CHECK_CLOSE(t2 - t, 0.044528007507, 1e-4);
 #else
   BOOST_WARN_MESSAGE(false, "Skipping AirMspiTimeTable test, because we haven't build ith MSPI_SHARED support");
 #endif

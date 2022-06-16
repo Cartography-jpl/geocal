6f9151602bf02a1a87ccb95a4465406901dfabc8
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2022-06-15 17:05:23 -0700

    Have the rest of boost_pcall.h working with boost::process.

--- lib/GeoCalCore/Implementation/rsm_nitf.cc
+++ lib/GeoCalCore/Implementation/rsm_nitf.cc
@@ -31,7 +31,6 @@ void GeoCal::rsm_write_nitf(const std::string& Fname,
 		    const boost::shared_ptr<Rsm>& R)
 {
   boost_pcall_noret("boost_nitf_rsm to_nitf " + Fname, R);
-  // boost_pcall_noret("/home/smyth/Local/geocal-build/install/bin/boost_nitf_rsm to_nitf " + Fname, R);
 }
 
 //-----------------------------------------------------------------------
--- lib/GeoCalCore/Support/boost_pcall.h
+++ lib/GeoCalCore/Support/boost_pcall.h
@@ -19,32 +19,61 @@ template<class In, class Out>
 boost::shared_ptr<Out> boost_pcall(const std::string& Command,
 				   const boost::shared_ptr<In>& Din)
 {
+  namespace bp = boost::process;
+  std::string data = serialize_write_binary(Din);
+  bp::opstream pin;
+  bp::ipstream pout;
+  // bp::child is low level, so it doesn't search on the path. Determine
+  // the location of the executable
+  auto pos = Command.find(" ");
+  std::string exec = bp::search_path(Command.substr(0,pos)).string();
+  if(pos != std::string::npos)
+    exec += Command.substr(pos);
+  bp::child c(exec, bp::std_in < pin, bp::std_out > pout);
+  pin << data;
+  pin.flush();
+  // Our various programs wait for stdin to have a EOF before running
+  // (since data is binary, no other marker). So explicitly close the
+  // pipe here.
+  pin.pipe().close();
+  std::stringstream d;
+  // Bit obscure looking, but this is idiomatic in C++ for reading
+  // all of a stream. This sucks up all the data the process writes
+  // out until it finishes and stdout gets an EOF when it is closed.
+  d << pout.rdbuf();
+  c.wait();
+  if(c.exit_code() != 0) {
+    Exception e;
+    e << "Trouble calling " << Command << " in boost_pcall";
+    throw e;
+  }
+  return serialize_read_binary_string<Out>(d.str());
 }
 
 template<class Out>
 boost::shared_ptr<Out> boost_pcall(const std::string& Command)
 {
-  FILE* f = popen(Command.c_str(), "r");
-  if(!f) {
+  namespace bp = boost::process;
+  bp::ipstream pout;
+  // bp::child is low level, so it doesn't search on the path. Determine
+  // the location of the executable
+  auto pos = Command.find(" ");
+  std::string exec = bp::search_path(Command.substr(0,pos)).string();
+  if(pos != std::string::npos)
+    exec += Command.substr(pos);
+  bp::child c(exec, bp::std_out > pout);
+  std::stringstream d;
+  // Bit obscure looking, but this is idiomatic in C++ for reading
+  // all of a stream. This sucks up all the data the process writes
+  // out until it finishes and stdout gets an EOF when it is closed.
+  d << pout.rdbuf();
+  c.wait();
+  if(c.exit_code() != 0) {
     Exception e;
     e << "Trouble calling " << Command << " in boost_pcall";
     throw e;
   }
-  std::string data;
-  data.reserve(1000);		// We'll need some space, so go ahead
-				// at set it aside.
-  int c = fgetc(f);
-  while(!feof(f)) {
-    data.push_back((char) c);
-    c = getc(f);
-  }
-  int status = pclose(f);
-  if(status) {
-    Exception e;
-    e << "Trouble calling " << Command << " in boost_pcall";
-    throw e;
-  }
-  return serialize_read_binary_string<Out>(data);
+  return serialize_read_binary_string<Out>(d.str());
 }
   
 template<class In>
@@ -54,13 +83,13 @@ void boost_pcall_noret(const std::string& Command,
   namespace bp = boost::process;
   std::string data = serialize_write_binary(Din);
   bp::opstream pin;
-  // This is low level, so it doesn't search on the path. Determine
+  // bp::child is low level, so it doesn't search on the path. Determine
   // the location of the executable
   auto pos = Command.find(" ");
   std::string exec = bp::search_path(Command.substr(0,pos)).string();
   if(pos != std::string::npos)
     exec += Command.substr(pos);
-  bp::child c(exec,  bp::std_in < pin);
+  bp::child c(exec, bp::std_in < pin);
   pin << data;
   pin.flush();
   // Our various programs wait for stdin to have a EOF before running
